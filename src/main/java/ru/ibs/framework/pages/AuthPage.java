100f2eca271cefe4a831eb96226c8ea3e577311a
Yuriy Vakhrushev <yura.vahrushev@yandex.ru>
2025-05-31 08:48:54 +0400

    Начал делать PageObject

--- src/main/java/ru/ibs/framework/pages/AuthPage.java
+++ src/main/java/ru/ibs/framework/pages/AuthPage.java
@@ -1,29 +1,31 @@
 package ru.ibs.framework.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 public class AuthPage {
 
     WebDriver driver;
 
-    @FindBy(xpath = "//form[@id='login-form']//fieldset")
-    WebElement loginForm;
+    @FindBy(xpath = "//form[@id='login-form']//fieldset//input[@id='prependedInput']")
+    WebElement inputLogin;
+    @FindBy(xpath = "//form[@id='login-form']//input[@id='prependedInput2']")
+    WebElement inputPass;
+    @FindBy(xpath = "//form[@id='login-form']//button[@id='_submit']")
+    WebElement loginButton;
 
 
-        public AuthPage(WebDriver driver) {
+
+    public AuthPage(WebDriver driver) {
             PageFactory.initElements(driver, this);
         }
 
         public void authorization (String login, String password) {
-        loginForm.findElement(By.xpath(".//input[@id='prependedInput']")).sendKeys(login);
-        loginForm.findElement(By.xpath(".//input[@id='prependedInput2']")).sendKeys(password);
-        loginForm.findElement(By.xpath(".//button[@id='_submit']")).click();
+        inputLogin.sendKeys(login);
+        inputPass.sendKeys(password);
+        loginButton.click();
         }
-
 
 }
